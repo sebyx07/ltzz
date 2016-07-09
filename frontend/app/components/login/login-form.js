4394ecb7ad4316d3c13a0fc48a10369a511b37f0
AlexLeonte <lad.leonte@gmail.com>
2016-07-09 21:09:59 +0300

    add the forms

--- frontend/app/components/login/login-form.js
+++ frontend/app/components/login/login-form.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'form',
-  classNames: ['m-t-md']
+  classNames: ['m-t-md'],
+
+  submit(e){
+    e.preventDefault();
+    const {username, password} = this.getProperties(['username', 'password']);
+    alert(username);
+  }
 });
