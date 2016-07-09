8d3a557d48b365d345f6a8bb71cb2c71156dfea0
sebi <gore.sebyx@yahoo.com>
2016-07-09 20:24:27 +0300

    migrate to postgresql

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20160709171519) do
 
-  create_table "invitations", id: nil, force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "invitations", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.integer  "user_id"
     t.boolean  "used"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["id"], name: "sqlite_autoindex_invitations_1", unique: true
-    t.index ["user_id"], name: "index_invitations_on_user_id"
+    t.index ["user_id"], name: "index_invitations_on_user_id", using: :btree
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -35,7 +38,9 @@ ActiveRecord::Schema.define(version: 20160709171519) do
     t.integer  "organization_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["organization_id"], name: "index_users_on_organization_id"
+    t.index ["organization_id"], name: "index_users_on_organization_id", using: :btree
   end
 
+  add_foreign_key "invitations", "users"
+  add_foreign_key "users", "organizations"
 end
