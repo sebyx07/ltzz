02e9c2e01990650888e65502d860ecb70d8f122c
sebi <gore.sebyx@yahoo.com>
2016-07-09 21:18:37 +0300

    change to head

--- backend/app/controllers/api/v1/session_controller.rb
+++ backend/app/controllers/api/v1/session_controller.rb
@@ -1,19 +1,19 @@
 module Api
   module V1
-    class SessionsController < ApplicationController
+    class SessionController < ApplicationController
       def create
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
           session[:user_id] = user.id
-          render nothing: true
+          head :ok
         else
-          render nothing: true, status: 422
+          head :not_found
         end
       end
 
       def destroy
         reset_session
-        render nothing: true
+        head :ok
       end
     end
   end
