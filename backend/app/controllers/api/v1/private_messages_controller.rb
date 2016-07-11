ba3f446517423d4403af6deddc8a9d67605a1e9a
sebi <gore.sebyx@yahoo.com>
2016-07-11 23:12:01 +0300

    add the private messages

--- backend/app/controllers/api/v1/private_messages_controller.rb
+++ backend/app/controllers/api/v1/private_messages_controller.rb
@@ -1,7 +1,16 @@
 module Api
   module V1
     class PrivateMessagesController < Api::V1::BaseController
+      def index
+        conv_id = params['filter']['conversation_id']
+        messages = PrivateMessage.where(sender_id: conv_id)
+        messages_serialized = messages.map {|m| Api::V1::PrivateMessageResource.new(m, nil)}
 
+        serialize = JSONAPI::ResourceSerializer.new(Api::V1::PrivateMessageResource)
+                        .serialize_to_hash(messages_serialized)
+
+        render json: serialize
+      end
     end
   end
 end
--- frontend/app/routes/organization/private-conversation.js
+++ frontend/app/routes/organization/private-conversation.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  currentUser: Ember.inject.service(),
   model(params){
     return this.store.query('user', {filter: {username: params.username}}).then((users) => {
-      return users.get('firstObject');
+      const user = users.get('firstObject');
+      Ember.$.ajax(`/api/v1/private-messages/${user.get('id')}`).then((data) => {
+        this.store.pushPayload(data);
+      });
     });
   }
 });
